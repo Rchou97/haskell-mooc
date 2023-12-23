d24caa8e37a291a572093321792f06c35bf7d3c0
Richard <93627768+Rchou97@users.noreply.github.com>
2023-12-23 23:51:45 +0100

    Add updates

--- chapter_1/Gold.hs
+++ chapter_1/Gold.hs
@@ -7,8 +7,10 @@ phi = (sqrt 5 + 1) / 2
 polynomial :: Double -> Double
 polynomial x = x^2 - x - 1
 
+f :: Double -> Double
 f x = polynomial (polynomial x)
 
+main :: IO ()
 main = do
   print (polynomial phi)
   print (f phi)
