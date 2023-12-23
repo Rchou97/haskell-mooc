ae4b6de8391dbbfdd5ddb3111d0a0520dc615545
Richard <93627768+Rchou97@users.noreply.github.com>
2023-12-23 23:52:00 +0100

    Add updates

--- chapter_1/chapter_1_hs.hs
+++ chapter_1/chapter_1_hs.hs
@@ -32,6 +32,20 @@ primes = sieve [2..]
   where
     sieve (p:xs) = p : sieve [x|x <- xs, x `mod` p > 0]
 
+-- shadowing
+x :: Int
+x = 5
+
+f :: Int -> Int
+f x = 2 * x
+
+g :: Int -> Int
+g y = x where x = 6
+
+h :: Int -> Int
+h x = x where x = 3
+
+
 main :: IO ()
 main = do
     print (lengthString)
@@ -41,3 +55,5 @@ main = do
     print (area (Circle 10))
     print (list)
     print (take 10 primes)
+
+
